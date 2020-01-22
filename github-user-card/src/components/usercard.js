f2981b7ee50dcf919c20cfcd9644d3881c46ee96
steven oakley <stevenoakley211@live.com>
2020-01-21 21:11:02 -0600

    MVP

--- github-user-card/src/components/usercard.js
+++ github-user-card/src/components/usercard.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Card = styled.div `
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    max-width: 300px;
+    margin: auto;
+    text-align: center;
+`
+const Img = styled.img`
+    width:100%;
+
+`
 
 class UserCard extends React.Component {
     render(){
         return(
-            <div>
+            <Card>
                 {console.log(this.props)}
                 <h1>{this.props.user.login}</h1>
-                <img src={this.props.user.avatar_url}></img>
+                <Img src={this.props.user.avatar_url}></Img>
                 <div>
                     <h2>Followers</h2>
                     {this.props.user_followers.map((item) => <p>{item.login}</p>)}
                 </div>
-            </div>
+            </Card>
         )
     }
 }
